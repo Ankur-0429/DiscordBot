bd4bff6df3683c9401ecc08499650aaf95a8edb6
Ankur Ahir <ahir.ankur0429@gmail.com>
2022-06-14 15:08:09 -0700

    added eric

--- commands/owoify.js
+++ commands/owoify.js
@@ -1,16 +1,19 @@
-const owoify = require('owoify-js').default
+const owoify = require("owoify-js").default;
 
 module.exports = {
-    name: 'owoify',
-    description: "",
-    execute(message){
-        if (message.author.bot) return;
-        const args2 = message.content.trim()
-        let owo = owoify(args2)
+  name: "owoify",
+  description: "",
+  execute(message) {
+    if (message.author.bot) return;
+    const args2 = message.content.trim();
+    let owo = owoify(args2);
 
-        if(message.author.username === 'sushirymushia'){
-            message.delete()            
-            return message.channel.send(owo).catch(e => console.log(e)) 
-        }
+    if (
+      message.author.username === "sushirymushia" ||
+      message.author.username === "glute i us max"
+    ) {
+      message.delete();
+      return message.channel.send(owo).catch((e) => console.log(e));
     }
-}
+  },
+};
